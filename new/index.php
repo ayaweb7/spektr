9b9a27431f30c5254edf355a5af6b0029cf4ece8
NikArt <ayaweb7@gmail.com>
2021-09-25 19:11:19 +0300

    Yandex metrika

--- new/index.php
+++ new/index.php
@@ -27,6 +27,22 @@
     <!--script src="https://kit.fontawesome.com/03ab4f6e6d.js" crossorigin="anonymous"></script-->
     <link rel="shortcut icon" type="image/ico" href="img/favicon.ico" />
 
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript" >
+   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+   ym(85535566, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true
+   });
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/85535566" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
+
 </head>
 
 <body>
